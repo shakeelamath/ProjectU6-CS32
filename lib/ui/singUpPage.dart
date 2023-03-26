43bfbc78b56b44f977486d3c62e71130cbba54cc
budwika <95086386+budwika@users.noreply.github.com>
2023-03-26 09:15:11 +0530

    Create Signup Page

--- lib/ui/singUpPage.dart
+++ lib/ui/singUpPage.dart
@@ -1,15 +1,120 @@
 import 'package:flutter/material.dart';
 
-class SignUpPage extends StatelessWidget{
+class SignupPage extends StatefulWidget {
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+
+  final _formKey = GlobalKey<FormState>();
+  final _userNameController = TextEditingController();
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: const Text('Sign in page '),
+    return MaterialApp(
+      title:'SignupPage',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Sign Up for μ6'),
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.pink,
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              // Navigate back to the previous screen or page
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    controller: _userNameController,
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                    ),
+                    validator: (value) {
+                      if (value != null && value.isEmpty) {
+                        return 'Please enter your Username';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      labelText: 'Name',
+                    ),
+                    validator: (value) {
+                      if (value != null && value.isEmpty) {
+                        return 'Please enter your name';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                    ),
+                    validator: (value) {
+                      if (value != null && value.isEmpty) {
+                        return 'Please enter your email';
+                      }
+                      // add more email validation here
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                    ),
+                    validator: (value) {
+                      if (value != null && value.isEmpty) {
+                        return 'Please enter your password';
+                      }
+                      // add more password validation here
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    child: Text('Sign Up'),
+                    onPressed: () {
+                      if (_formKey.currentState != null && _formKey.currentState!.validate()) {
+                        // form is valid, do something here
+                        // e.g. submit the form data to server
+                        // or save the data to local storage
+                        String username = _userNameController.text;
+                        String name = _nameController.text;
+                        String email = _emailController.text;
+                        String password = _passwordController.text;
+                        // add your own implementation here
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
-
-
   }
-
 }
